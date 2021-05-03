ead1ff83437c364b49c72c3eebac28bb275f5b33
ADSmith-0 <adam00199382818@gmail.com>
2021-05-03 13:53:15 +0100

    artist schema implemented

--- server/index.js
+++ server/index.js
@@ -1,5 +1,37 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT | 5000;
-app.listen(PORT, `Server listening on port ${PORT}`);
+require('dotenv').config();
+
+console.log(process.env.MONGO_URI);
+
+// For parsing application/json 
+app.use(express.json());
+
+// Connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+.then(console.log('Connected to MongoDB'))
+.catch(err => console.error(err));
+
+// CORS access 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+    next();
+});
+
+// Enable CORS requests
+app.options('*/*', (req, res) => {
+    res.status(200).end();
+});
+
+app.use('/api/artists', require('./routes/api/artists.js'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => `Server listening on port ${PORT}`);
